f66b41382df684afa5f7df2dc7a5ec5a210879ed
DeadMrts <lalis.costa.martins@gmail.com>
2025-12-04 19:57:17 -0300

    teste

---
+++ Prova/AppDataContext.cs
@@ -0,0 +1,9 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options)
+        : base(options) {}
+
+}
